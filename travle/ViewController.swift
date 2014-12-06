c9d2c77429480caf87f4f4f0865720d1e0f59a3e
barakawei <barakawei@me.com>
2014-12-06 10:08:35 +0800

    Initial Commit

---
+++ travle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travle
+//
+//  Created by Qian Xiaowei on 14-12-6.
+//  Copyright (c) 2014年 baraka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
